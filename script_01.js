5a292749835432b4d1331851f96f9722c726dbcb
Roxana <roxanarevilla@outlook.de>
2023-05-12 11:47:25 +0200

    2. Auswahl Rechenart : check !

--- script_01.js
+++ script_01.js
@@ -6,24 +6,61 @@
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
 
 1. Dateneingabe + -überprüfung : 
-2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+2. Auswahl Rechenart : check !
+3. Fkt. Grundrechenarten : check !
 4. Ausgabe in Konsole : chenk !
 */
 
+// Konstanten
+const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
+const ERROR_STR_GEN = "Irgendetwas ging schief!"
+
+
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+output(calculator(3,2,"+"));
+output(calculator(3,2,"-"));
+output(calculator(3,2,"*"));
+output(calculator(3,2,":"));
+output(calculator(3,2,"/"));
+output(calculator(3,0,"/"));
+output(calculator(3,2,"#?!"));
+function calculator(a,b,op) {
+switch (op) {
+
+	case "+":
+		return add(a,b);
+
+		case "-":	
+		return subtract(a,b);
+
+		case "*":
+			return multiply(a,b);
+
+			case "/":
+			case ":":
+				return divide(a,b);
+
+
+	default:
+return ERROR_STR_GEN;
+
+}
+
+}
 
 // module: division a / b |  test:
-output(divide(4,2));
-output(divide(3,2));
-output(divide(3,-2));
-output(divide(3,0));
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(3,0));
 function divide(a,b) {
 
-	if (b != 0) {
-		return a/b;
-	} else {
-		return "Teilen durch 0 nicht möglich";
+	if (b == 0) { // Ausnahme + Abbruch
+		return ERROR_STR_DIV;
 	}
+		return a/b; //Regel 
+	
 	
 
 }
