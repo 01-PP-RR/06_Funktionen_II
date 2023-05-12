2486b5b79474b4c73d269b2632510d7d8e3ee667
Roxana <roxanarevilla@outlook.de>
2023-05-12 10:44:02 +0200

    // module: division a / b |  test:

--- script_01.js
+++ script_01.js
@@ -8,16 +8,63 @@
 1. Dateneingabe + -überprüfung : 
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
+4. Ausgabe in Konsole : chenk !
 */
+
+
+// module: division a / b |  test:
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(3,0));
+function divide(a,b) {
+
+	if (b != 0) {
+		return a/b;
+	} else {
+		return "Teilen durch 0 nicht möglich";
+	}
+	
+
+}
+
+
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function multiply(a,b){
+	return a*b;
+}
+// output(subtract(2,2));
+// output(subtract(2,-2));
+// output(subtract(2,0));
+
+
+// module: subtraction a - b |  test:
+function subtract(a,b) {
+	return a-b;
+	
+}
+
+// module: addition a + b |  test:
+function add(a,b) {
+
+}
+
+
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+function add(a,b) {
+	// return a + b;	
+}
 
 
 
 
 
 // module: output | test:
-// output("hello");
-// output(2);
 function output(outputData) {
 	console.log(outputData);
 }
